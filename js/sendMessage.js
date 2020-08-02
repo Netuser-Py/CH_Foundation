21312c2db34fbddbbb172e3dc98d1d4f6db673c4
netuser_py <netuser-biolyrics@usa.net>
2020-08-02 00:11:35 -0600

    Update Contact us

---
+++ js/sendMessage.js
@@ -0,0 +1,16 @@
+// http://jsfiddle.net/228d4snb/
+
+var message = "";
+
+$("#sendMessage").on("click", function() {
+    message = $("#contactform").serialize();
+    $.ajax({
+        url: "https://formspree.io/mnqglkeb", 
+        method: "POST",
+        data: {message: message},
+        dataType: "json"
+    });
+    alert('Thanks for the email, we\'ll be in touch soon.');
+    window.open("https://chebibfoundation.org/", "_self");
+    return false;
+});	
